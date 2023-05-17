d3d30e1f342c8d99499b1aec302e0ba0645d7e7e
YarullinRS <yarullinrs@aquilon-st.ru>
2023-05-17 17:22:08 +0300

    init alglib

---
+++ OptimizationPackage/OptimizationPackage.cpp
@@ -0,0 +1,14 @@
+﻿// OptimizationPackage.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "optimization.h"
+#include <math.h>
+
+using namespace std;
+using namespace alglib;
+
+int main()
+{
+	return 0;
+}
